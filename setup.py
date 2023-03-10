ec061b04c76f28d8cfeb1e8ac26a890eba28b127
XinGuu <761119174@qq.com>
2023-03-09 19:20:26 -0500

    adapt notebook

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='CTAB-GAN',
+    version='1.0',
+    description='CTAB-GAN',
+    url='https://github.com/XinGuu/CTAB-GAN',
+    author='Anonymous',
+    license='MIT',
+    packages=['src'],
+    zip_safe=False,
+    install_requires=['numpy==1.21.0', 'torch==1.9.1', 'pandas==1.2.4', 'scikit-learn==0.24.1',
+                      'dython==0.6.4.post1', 'scipy==1.4.1'],
+)
